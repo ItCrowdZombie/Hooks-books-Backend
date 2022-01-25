8599bb96a483a8230f26b023ae1d44db08e08e69
MayuRuru <aruizrull@protonmail.com>
2022-01-25 12:14:15 +0100

    BookRepository

--- hookbook/src/main/java/com/itcrowd/hookbook/repository/BookRepository.java
+++ hookbook/src/main/java/com/itcrowd/hookbook/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.itcrowd.hookbook.repository;
 
-public interface BookRepository {
+import com.itcrowd.hookbook.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository <Book, Integer>{
+
 }
