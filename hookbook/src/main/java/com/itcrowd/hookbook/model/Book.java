c1533b8d98257e566683af67ed44d92034e1448d
SergiFuentes <you@example.com>
2022-01-25 11:59:43 +0100

    setting model

--- hookbook/src/main/java/com/itcrowd/hookbook/model/Book.java
+++ hookbook/src/main/java/com/itcrowd/hookbook/model/Book.java
@@ -1,4 +1,71 @@
 package com.itcrowd.hookbook.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.awt.*;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String title;
+    private String author;
+    private Image img;
+    private String description;
+    private int price;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
